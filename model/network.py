1f7750bf53cb064d302e93bf51cbedfe27f6c43e
Wei Jiangning <weijiangning@icloud.com>
2023-08-14 11:59:27 +0800

    tag_01: feat(ASE-GCN) add transformer for token: T*V ASE-GCN 在第一层中间直接加入transformer encoder 结构

--- model/network.py
+++ model/network.py
@@ -264,7 +264,7 @@ class Model(nn.Module):
                                   **kwargs, temporal_len=frame_len, fea_dim=c1, to_use_hyper_conv=True,
                                   activation=nonlinear)
         # TODO add something like transformer
-        self.transformer1 = TransformerEncoder(num_layers=3, d_model=c1, num_heads=8, d_ff=c1 * 2)
+        self.transformer1 = TransformerEncoder(num_layers=1, d_model=c1, num_heads=2, d_ff=c1 * 2)
 
         self.sgcn1_ms_tcn_1 = MS_TCN(c1, c1, activation=nonlinear)
         self.sgcn1_ms_tcn_2 = MS_TCN(c1, c1, activation=nonlinear)
@@ -332,7 +332,7 @@ class Model(nn.Module):
         ###### First Component ######
         x = self.sgcn1_msgcn(x)  # N*M C T V -> N*M C1 T V
 
-        # test transformer: single head attention
+        # test transformer: multi head attention
         x = x.permute(0, 2, 3, 1).contiguous().view(N * M, T * V, self.c1)
         x = self.transformer1(x)
         x = x.view(N * M, T, V, self.c1).permute(0, 3, 1, 2).contiguous()
